6aa89f3229b02cb6d20b6c84bde3ce4256b76732
Matthew Malensek <matt@malensek.net>
2024-01-04 12:52:28 -0800

    Wait for interrupt when no processes runnable

--- kernel/proc.c
+++ kernel/proc.c
@@ -452,6 +452,7 @@ scheduler(void)
     // Avoid deadlock by ensuring that devices can interrupt.
     intr_on();
 
+    int found_runnable = 0;
     for(p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
       if(p->state == RUNNABLE) {
@@ -465,8 +466,14 @@ scheduler(void)
         // Process is done running for now.
         // It should have changed its p->state before coming back.
         c->proc = 0;
+        found_runnable = 1;
       }
       release(&p->lock);
+    }
+
+    if (found_runnable == 0) {
+      intr_on();
+      asm volatile("wfi");
     }
   }
 }
